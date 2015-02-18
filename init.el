33c08d24fdc16b3e69d2d13fec6904278ebf8f5e
James Kruth <james@arsmentis.com>
2015-02-17 23:37:03 -0500

    Add the boilerplate for init.el

---
+++ init.el
@@ -0,0 +1,16 @@
+;;; init.el --- custom config entry point
+
+;; Copyright (c) 2015 James Kruth
+
+;; Author: James Kruth <james@kruth.org>
+;; URL: https://artlogic.github.io/.emacs.d/
+;; Version: 0.1
+;; Keywords: config
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; This file is the entry point for my customized configuration.
+
+;;; Code:
