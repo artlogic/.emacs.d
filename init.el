f1f7c17e0159278f4339e725e990679ebeaa849d
James Kruth <james@arsmentis.com>
2015-02-25 21:42:38 -0500

    Adding some licensing details and cleaning up the file to conform to: https://www.gnu.org/software/emacs/manual/html_node/elisp/Library-Headers.html#Library-Headers

--- init.el
+++ init.el
@@ -3,11 +3,23 @@
 ;; Copyright (c) 2015 James Kruth
 
 ;; Author: James Kruth <james@kruth.org>
-;; URL: https://artlogic.github.io/.emacs.d/
+;; Created: 17 Feb 2015
 ;; Version: 0.1
 ;; Keywords: config
+;; Homepage: https://artlogic.github.io/.emacs.d/
 
 ;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the MIT License.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the MIT
+;; License for more details.
+
+;; You should have received a copy of the MIT License along with this
+;; program. If not, see <http://opensource.org/licenses/MIT>.
 
 ;;; Commentary:
 
@@ -18,3 +30,5 @@
 ;; save customizations in custom.el
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'missing-ok)
+
+;;; init.el ends here
